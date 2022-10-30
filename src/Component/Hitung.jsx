c33aead7bb131e4521088d51363af492a7b41728
David Nasrulloh <davidkrb52@gmail.com>
2022-10-30 20:22:02 +0700

    feat: menambah fitur add and remove di perhitungan patungin

--- src/Component/Hitung.jsx
+++ src/Component/Hitung.jsx
@@ -2,26 +2,48 @@ import React from "react";
 import "./Hitung.css"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { useState } from "react";
 
-
-const InputItems = () => (
-    <div className="flex">
-        <form className="flex">
-            <div className="flex-1">
-                <input type="text" className="w-80 py-6 px-8 m-4 h-8 border-primary border-2 rounded-xl text-primary text-lg" value="Hahha" />
-            </div>
-            <div className="flex-1">
-                <input type="text" className="w-80 py-6 px-8 m-4 h-8 border-primary border-2 rounded-xl text-primary text-lg" value="Hahha" />
-            </div>
-            <div className="flex-1">
-                <input type="text" className="w-80 py-6 px-8 m-4 h-8 border-primary border-2 rounded-xl text-primary text-lg" value="Hahha" />
-            </div>
-        </form>
-        <button className="ml-4"><FontAwesomeIcon icon={faTrash} className="text-red-600 text-2xl"/></button>
-    </div>
-)
 
 const Hitung = () => {
+
+    const [itemsList, setItemsList] = useState([{
+        namaItem: "", 
+        jumlahItem: 0, 
+        hargaItem: 0
+    }]);
+
+    const [jumlahMember, setJumlahMember] = useState(0);
+
+    const handleJumlahMemberChange = (ev) => {
+        setJumlahMember(ev.target.value);
+        // console.log(jumlahMember);
+    }
+
+    const handleItemChange = (e, index) => {
+        const { name, value } = e.target;
+        const list = [...itemsList];
+        list[index][name] = value;
+        setItemsList(list);
+        console.log(list);
+    }
+
+    const handleItemRemove = (index) => {
+        const list = [...itemsList];
+        list.splice(index, 1);
+        setItemsList(list);
+        // console.log(list);
+    }
+
+    const handleItemAdd = () => {
+        setItemsList([...itemsList, {
+            namaItem: "", 
+            jumlahItem: 0, 
+            hargaItem: 0
+        }])
+    }
+
+    console.log(itemsList[0].hargaItem);
     return(
         <>
             <div className="mb-20">
@@ -29,10 +51,10 @@ const Hitung = () => {
                     {/* <hr className="mb-16"/> */}
                     <div className="flex">
                         <div className="w-3/4 mr-12">
-                            <div className="flex justify-between items-center">
-                                <h1 className="font-bold text-primary text-3xl">Coba Hitung Patunganmu !</h1>
-                                <button className="mr-8 py-4 px-8 bg-primary text-white text-lg font-bold rounded-xl"><FontAwesomeIcon icon={faPlus} className="mr-4" />Tambah Items</button>
+                            <div className="flex justify-between items-center mb-12">
+                                <h1 className="font-bold text-primary text-3xl">Coba Hitung Patunganmu !</h1>                                
                             </div>
+                            {/* Header Judul */}
                             <div className="flex flex-row text-center mt-6 font-bold text-xl text-primary mb-4 mr-12">
                                 <div className="flex-1">
                                     Nama Item
@@ -44,16 +66,89 @@ const Hitung = () => {
                                     Harga
                                 </div>
                             </div>
-                            <div className="">
-                                <InputItems/>
-                            </div>
+                            {/* Input Items */}
+                            <form className="" autoComplete="off">
+                                {
+                                    itemsList.map((singleItems, index) => (
+                                        <div className="flex" key={index}>
+                                            <div className="first-division text-center">
+                                                <div className="flex">
+                                                    <div className="flex-1">
+                                                        <input 
+                                                            type="text" 
+                                                            name="namaItem" 
+                                                            id="namaItem" 
+                                                            className="w-80 py-6 px-8 m-4 h-8 border-primary border-2 rounded-xl text-primary text-lg" 
+                                                            placeholder="Masukkan nama item" 
+                                                            autoComplete="off" 
+                                                            value={singleItems.namaItem}
+                                                            onChange={(ev)=>handleItemChange(ev, index)}
+                                                            required
+                                                            />
+                                                    </div>
+                                                    <div className="flex-1">
+                                                        <input 
+                                                            type="number" 
+                                                            name="jumlahItem" 
+                                                            id="jumlahItem" 
+                                                            className="w-80 py-6 px-8 m-4 h-8 border-primary border-2 rounded-xl text-primary text-lg" 
+                                                            placeholder="Jumlah item" 
+                                                            value={singleItems.jumlahItem}
+                                                            onChange={(ev)=>handleItemChange(ev, index)}
+                                                            required
+                                                            />
+                                                    </div>
+                                                    <div className="flex-1">
+                                                        <input 
+                                                            type="number" 
+                                                            name="hargaItem" 
+                                                            id="hargaItem" 
+                                                            className="w-80 py-6 px-8 m-4 h-8 border-primary border-2 rounded-xl text-primary text-lg" 
+                                                            placeholder="Harga per item" 
+                                                            value={singleItems.hargaItem}
+                                                            onChange={(ev)=>handleItemChange(ev, index)}
+                                                            required/>
+                                                    </div>
+                                                    {
+                                                        itemsList.length !== 1 && (
+                                                            <button 
+                                                                type="button"
+                                                                onClick={() => handleItemRemove(index)}
+                                                                className="">
+                                                                <FontAwesomeIcon icon={faTrash} className="text-red-600 text-2xl"/>
+                                                                {/* <span>Remove</span> */}
+                                                            </button>
+                                                            
+                                                        )
+                                                    }
+                                                </div>
+                                                {
+                                                    itemsList.length - 1 === index && itemsList.length < 10 && (
+                                                        
+                                                        <button className="mr-8 py-2 px-8 mt-4 bg-primary text-white text-lg font-bold rounded-xl" type="button"
+                                                            onClick={handleItemAdd}><FontAwesomeIcon icon={faPlus} className="mr-4" />Tambah Items</button>
+                                                    )
+                                                }
+                                            </div>       
+                                        </div>
+                                    ))
+                                }
+                            </form>
                         </div>
                         <div className="w-1/4 flex flex-col items-center border-l-4">
                             <h4 className="text-primary font-bold text-2xl">Jumlah Anggota Patungan</h4>
-                            <input type="text" className=" my-8 border-4 border-primary rounded-xl py-24 px-16 w-44 text-center text-5xl text-primary font-bold" value={0} />
+                            <div className="my-auto">
+                            <input 
+                                type="text" 
+                                className=" my-8 border-4 border-primary rounded-xl py-24 w-48 text-center text-5xl text-primary font-bold" 
+                                name="jumlahMember"
+                                value={jumlahMember}
+                                onChange={(evt)=>handleJumlahMemberChange(evt)} />
+                            </div>
                         </div>
                     </div>
                     <hr className="border-2 mb-8 mt-8"/>
+                    {/* Hasil nya nanti */}
                     <div className="my-8 flex justify-start items-center">
                         <p className="text-xl px-8 py-4 rounded-lg bg-primary text-white w-1/5">Total Patungan Per Orang </p>
                         <p className="text-2xl font-bold mx-12 text-primary"> : Hasil nya nanti disini</p>
