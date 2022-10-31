0fb1172f8fa46fd80971e9b2d4e934c0754bb2ea
David Nasrulloh <davidkrb52@gmail.com>
2022-10-31 15:32:02 +0700

    feat: menambah responsive design pada mobile

--- src/Component/Hitung.jsx
+++ src/Component/Hitung.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import "./Hitung.css"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faPlus, faTrash, faCalculator } from '@fortawesome/free-solid-svg-icons';
 import { useState } from "react";
 // import { setNamaItem, setJumlahItem, setHargaItem, setListData } from "../features/hitungSlice";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
 
 
 const Hitung = ({
@@ -17,14 +16,15 @@ const Hitung = ({
 
     const [jumlahMember, setJumlahMember] = useState(0);
     const [jumlahPatungan, setJumlahPatungan] = useState(0);
+    let totalHargaBarang = 0;
 
     const handleViewTotalPatungan = () => {
-        let totalHargaBarang = 0;
         itemsList.map((item)=>(
             totalHargaBarang += +item.jumlahItem * +item.hargaItem 
         ))
         setJumlahPatungan(totalHargaBarang/jumlahMember);
     }
+
 
     const handleJumlahMemberChange = (ev) => {
         setJumlahMember(ev.target.value);
@@ -33,16 +33,16 @@ const Hitung = ({
 
     return(
         <>
-            <div className="mb-20">
+            <div className="mb-20 max-[620px]:mb-12">
                 <div className="container mx-auto">
                     {/* <hr className="mb-16"/> */}
-                    <div className="flex">
-                        <div className="w-3/4 mr-12">
+                    <div className="flex max-[620px]:flex-col max-[620px]:text-center max-[620px]:px-0">
+                        <div className="w-3/4 mr-12 max-[620px]:mr-2 max-[620px]:w-full">
                             <div className="flex justify-between items-center mb-12">
-                                <h1 className="font-bold text-primary text-3xl">Coba Hitung Patunganmu !</h1>                                
+                                <h1 className="font-bold text-primary text-3xl max-[620px]:px-12">Coba Hitung Patunganmu !</h1>                                
                             </div>
                             {/* Header Judul */}
-                            <div className="flex flex-row text-center mt-6 font-bold text-xl text-primary mb-4 mr-12">
+                            <div className="flex flex-row text-center mt-6 font-bold text-xl text-primary mb-4 mr-12 max-[620px]:hidden">
                                 <div className="flex-1">
                                     Nama Item
                                 </div>
@@ -57,9 +57,9 @@ const Hitung = ({
                             <form className="" autoComplete="off">
                                 {
                                     itemsList.map((singleItems, index) => (
-                                        <div className="flex" key={index}>
-                                            <div className="first-division text-center">
-                                                <div className="flex">
+                                        <div className="flex max-[620px]:justify-center" key={index}>
+                                            <div className="first-division text-center max-[620px]:mb-8">
+                                                <div className="flex max-[620px]:flex-col">
                                                     <div className="flex-1">
                                                         <input 
                                                             type="text" 
@@ -109,7 +109,7 @@ const Hitung = ({
                                                 {
                                                     itemsList.length - 1 === index && itemsList.length < 10 && (
                                                         
-                                                        <button className="mr-8 py-2 px-8 mt-4 bg-primary text-white text-lg font-bold rounded-xl" type="button"
+                                                        <button className="mr-8 py-2 px-8 mt-4 bg-primary text-white text-lg font-bold rounded-xl max-[620px]:mr-0" type="button"
                                                             onClick={handleItemAdd}><FontAwesomeIcon icon={faPlus} className="mr-4" />Tambah Items</button>
                                                     )
                                                 }
@@ -119,8 +119,8 @@ const Hitung = ({
                                 }
                             </form>
                         </div>
-                        <div className="w-1/4 flex flex-col items-center border-l-4">
-                            <h4 className="text-primary font-bold text-2xl">Jumlah Anggota Patungan</h4>
+                        <div className="w-1/4 flex flex-col items-center border-l-4 max-[620px]:w-full max-[620px]:border-t-4 max-[620px]:border-l-0">
+                            <h4 className="text-primary font-bold text-2xl max-[620px]:px-12 max-[620px]:w-full max-[620px]:py-8">Jumlah Anggota Patungan</h4>
                             <div className="my-auto">
                             <input 
                                 type="text" 
@@ -131,13 +131,13 @@ const Hitung = ({
                             </div>
                         </div>
                     </div>
-                    <hr className="border-2 mb-8 mt-8"/>
+                    <hr className="border-b-4 mb-8 mt-8"/>
                     {/* Hasil nya nanti */}
-                    <div className="my-8 flex justify-start items-center">
-                        <button className="text-xl px-8 py-4 rounded-lg bg-primary text-white w-1/3" onClick={handleViewTotalPatungan}>Tampilkan Total Patungan Per Orang </button>
-                        <p className="text-2xl font-bold mx-12 text-primary"> : {jumlahPatungan}</p>
+                    <div className="my-8 flex justify-start items-center max-[620px]:flex-col">
+                        <button className="text-xl py-4 rounded-lg bg-primary text-white w-1/4 max-[620px]:w-3/4 max-[620px]:mb-8 max-[620px]:px-2 max-[620px]:text-lg" onClick={handleViewTotalPatungan}><FontAwesomeIcon icon={faCalculator} className="mr-4" /> Hitung Patungan Per Orang </button>
+                        <p className="text-4xl font-bold mx-12 text-primary"> : {jumlahPatungan} / Orang</p>
                     </div>
-                    <hr className="border-2 mb-8"/>
+                    <hr className="border-t-4 mb-8"/>
                     <div className="flex justify-center">
                         <h2 className="w-2/4 leading-normal text-center text-3xl text-primary font-normal">Pengen nyoba custom patungan atau scan bill mu? Dapetin fiturnya di <span className="font-bold">Patungin</span> aja! </h2>
                     </div>
--- src/TemplatePage/Footer/FooterContainer.jsx
+++ src/TemplatePage/Footer/FooterContainer.jsx
@@ -17,34 +17,33 @@ const Footer = () => {
         scroll.scrollToTop();
     }
 
-
     return(
         <div className="bg-primary mt-44">
             <div className="container mx-auto">
-                <div className="flex flex-row px-36 h-60">
-                    <div className="flex-auto w-20">
+                <div className="flex flex-row px-36 h-60 max-[620px]:h-auto max-[620px]:mb-8 max-[620px]:px-[0px] max-[620px]:flex-col ">
+                    <div className="flex-auto w-20 max-[620px]:w-32">
                         <div className="flex flex-row justify-content-start align-items-center">
                             {/* <p className="object-center">cok</p> */}
-                            <img src={gambar1} alt="Gambar App Patungin" className="w-44 gambar-phone left-auto ml-40 drop-shadow-xl rounded-2xl" />
+                            <img src={gambar1} alt="Gambar App Patungin" className="w-44 gambar-phone left-auto ml-40 drop-shadow-xl rounded-2xl max-[620px]:ml-32" />
                         </div>
                     </div>
-                    <div className="flex-auto w-96 text-white pt-20 pb-12">
-                        <h2 className="text-3xl font-bold mb-2">Patungan #MUDAHBANGET Sekarang!</h2>
+                    <div className="flex-auto w-96 text-white pt-20 max-[620px]:pt-2 max-[620px]:px-12">
+                        <h2 className="text-3xl font-bold mb-2 max-[620px]:text-lg">Patungan #MUDAHBANGET Sekarang!</h2>
                         <p>Download Patungin Sekarang</p>
-                        <div className="mt-4 flex" >
-                            <img src={aStore} alt="App Store" className="w-32 mr-3"/>
-                            <img src={gPlay} alt="Google Play" className="w-32 mr-3"/>
-                            <img src={appGalery} alt="App Galery" className="w-32"/>
+                        <div className="mt-4 flex max-[620px]:flex max-[620px]:flex-col" >
+                            <img src={aStore} alt="App Store" className="w-32 mr-3 pb-2"/>
+                            <img src={gPlay} alt="Google Play" className="w-32 mr-3 pb-2"/>
+                            <img src={appGalery} alt="App Galery" className="w-32 pb-2"/>
                         </div>
                     </div>
                 </div>
                 <div className="">
                     <hr/>
                 </div>
-                <div className="flex justify-between py-12">
-                    <div>
-                        <h3 className="text-4xl font-bold text-white">Patungin</h3>
-                        <ul className="footer-menu text-lg">
+                <div className="flex justify-between py-12 max-[620px]:container mx-auto max-[620px]:flex max-[620px]:flex-col max-[620px]:text-center">
+                    <div className="max-[620px]:px-12">
+                        <h3 className="text-4xl font-bold text-white max-[620px]:text-xl">Patungin</h3>
+                        <ul className="footer-menu text-lg max-[620px]:text-sm max-[620px]:flex-col max-[620px]:mb-8">
                             <li>
                                 <Link to="/" onClick={scrollToTop} smooth={true} offset={50} duration={300} delay={500}>
                                     Home
@@ -66,12 +65,12 @@ const Footer = () => {
                                 </Link>
                             </li>
                         </ul>
-                        <p className="text-lg text-white mt-2">© 2022 Patungin - PT. Asix Debit Indonesia Koe. All Rights Reserved.</p>
+                        <p className="text-lg text-white mt-2 max-[620px]:text-sm">© 2022 Patungin - PT. Asix Debit Indonesia Koe. All Rights Reserved.</p>
                     </div>
                     <div>
-                        <div className="flex w-96 justify-end">
+                        <div className="flex w-96 justify-end max-[620px]:justify-center max-[620px]:mt-6">
                             <div className="social-media">
-                                <p className="font-bold text-white mb-5">Our Social Media</p>
+                                <p className="font-bold text-white mb-5 max-[620px]:text-center max-[620px]:text-sm">Our Social Media</p>
                                 <div className="flex social-media-icons">
                                     <img src={instagram} alt="instagram" />
                                     <img src={facebook} alt="facebook" />
